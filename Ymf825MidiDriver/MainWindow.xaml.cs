96a3aeee876cef2fad867bd82525df52e6dceff1
Tomona Nanase <nanase@users.noreply.github.com>
2017-10-26 00:25:41 +0900

    MainWindowの実装変更
    - プロジェクトの保存、読み込み、新規作成を追加
    - トーンの作成、削除、複製を追加
    - ツールチップバーの一部実装
    - バインドの実装
    - コマンドの追加、実装
    - IsEnabledがfalseになったときに半透明にするよう変更

--- Ymf825MidiDriver/MainWindow.xaml.cs
+++ Ymf825MidiDriver/MainWindow.xaml.cs
@@ -1,25 +1,345 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Ymf825MidiDriver
 {
-    /// <inheritdoc cref="Window" />
+    /// <inheritdoc cref="System.Windows.Window" />
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow
     {
-        public static readonly DependencyProperty BasicOctaveProperty = DependencyProperty.Register("BasicOctave", typeof(int), typeof(MainWindow), new PropertyMetadata(1));
+        #region -- Public Fields --
 
-        public int BasicOctave
+        public static readonly RoutedCommand NewToneCommand = new RoutedCommand();
+        public static readonly RoutedCommand DeleteToneCommand = new RoutedCommand();
+        public static readonly RoutedCommand DuplicateToneCommand = new RoutedCommand();
+        public static readonly DependencyProperty SelectedToneItemProperty = DependencyProperty.Register("SelectedToneItem", typeof(ToneItem), typeof(MainWindow), new PropertyMetadata(default(ToneItem)));
+
+        #endregion
+
+        #region -- Private Fields --
+
+        private readonly ObservableCollection<ToneItem> toneItems;
+
+        private string filePath;
+        private string fileName;
+        private bool fileChanged;
+        private bool isMonitoringChanging;
+
+        #endregion
+
+        #region -- Public Properties --
+
+        public ToneItem SelectedToneItem
         {
             // ReSharper disable once PossibleNullReferenceException
-            get => (int)GetValue(BasicOctaveProperty);
-            set => SetValue(BasicOctaveProperty, value);
+            get => (ToneItem)GetValue(SelectedToneItemProperty);
+            set => SetValue(SelectedToneItemProperty, value);
         }
+
+        #endregion
+
+        #region -- Constructors --
 
         public MainWindow()
         {
             InitializeComponent();
+            toneItems = new ObservableCollection<ToneItem>();
+            DataContext = toneItems;
+
+            toneItems.CollectionChanged += ToneItemsOnCollectionChanged;
+            UpdateWindowTitle();
         }
+
+        #endregion
+
+        #region -- Private Methods --
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            isMonitoringChanging = true;
+        }
+
+        private void ToneItemsOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (!isMonitoringChanging || fileChanged)
+                return;
+
+            fileChanged = true;
+            UpdateWindowTitle();
+        }
+
+        private void TonePropertyChanged(object sender, RoutedEventArgs e)
+        {
+            if (isMonitoringChanging && !fileChanged)
+            {
+                fileChanged = true;
+                UpdateWindowTitle();
+            }
+
+            e.Handled = true;
+        }
+
+        private void UpdateWindowTitle()
+        {
+            Title = $"{(fileChanged ? "*" : "")}{fileName ?? "untitled"} ― YMF825 MIDI Driver";
+        }
+
+        private void Command_CanAlwaysExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            var toolBar = sender as ToolBar;
+
+            if (toolBar?.Template.FindName("OverflowGrid", toolBar) is FrameworkElement overflowGrid)
+                overflowGrid.Visibility = Visibility.Collapsed;
+
+            if (toolBar?.Template.FindName("MainPanelBorder", toolBar) is FrameworkElement mainPanelBorder)
+                mainPanelBorder.Margin = new Thickness(0);
+        }
+
+        private void NewToneCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            toneItems.Add(new ToneItem());
+            ToneListBox.SelectedIndex = toneItems.Count - 1;
+        }
+
+        private void DeleteToneCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var index = ToneListBox.SelectedIndex;
+            toneItems.RemoveAt(index);
+
+            if (toneItems.Count == index)
+                index--;
+
+            ToneListBox.SelectedIndex = index;
+        }
+
+        private void DuplicateToneCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var index = ToneListBox.SelectedIndex;
+            var clone = toneItems[index].DeepClone();
+            clone.ProgramNumber = 0;
+            clone.ProgramNumberAssigned = false;
+            clone.PercussionNumber = 0;
+            clone.PercussionNumberAssigned = false;
+            toneItems.Add(clone);
+            ToneListBox.SelectedIndex = toneItems.Count - 1;
+        }
+
+        private void SaveCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (filePath == null)
+            {
+                if (ShowSaveDialog())
+                    SaveProject();
+            }
+            else
+                SaveProject();
+        }
+
+        private void SaveAsCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (ShowSaveDialog())
+                SaveProject();
+        }
+
+        private void OpenCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (fileChanged)
+            {
+                var confirm = ConfirmDiscordChanging();
+
+                switch (confirm)
+                {
+                    case true:
+                        if (filePath == null)
+                        {
+                            if (ShowSaveDialog())
+                                SaveProject();
+                            else
+                                return;
+                        }
+                        else
+                            SaveProject();
+                        break;
+
+                    case null:
+                        return;
+                }
+            }
+
+            if (ShowOpenDialog())
+                LoadProject();
+        }
+
+        private bool ShowSaveDialog()
+        {
+            var dialog = new SaveFileDialog()
+            {
+                Title = "名前を付けて保存",
+                Filter = "トーンプロジェクト (*.json)|*.json"
+            };
+
+            if (dialog.ShowDialog() != true)
+                return false;
+
+            filePath = dialog.FileName;
+            fileName = Path.GetFileName(filePath);
+            return true;
+        }
+
+        private bool ShowOpenDialog()
+        {
+            var dialog = new OpenFileDialog()
+            {
+                Title = "開く",
+                Filter = "トーンプロジェクト (*.json)|*.json"
+            };
+
+            if (dialog.ShowDialog() != true)
+                return false;
+
+            filePath = dialog.FileName;
+            fileName = Path.GetFileName(filePath);
+            return true;
+        }
+
+        private void SaveProject()
+        {
+            var serializeObject = JsonConvert.SerializeObject(toneItems, new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+            File.WriteAllText(filePath, serializeObject);
+
+            fileChanged = false;
+            UpdateWindowTitle();
+        }
+
+        private void LoadProject()
+        {
+            var serializeText = File.ReadAllText(filePath);
+            var toneItemsObject = JsonConvert.DeserializeObject<IEnumerable<ToneItem>>(serializeText);
+
+            isMonitoringChanging = false;
+
+            toneItems.Clear();
+
+            foreach (var item in toneItemsObject)
+                toneItems.Add(item);
+
+            fileChanged = false;
+            UpdateWindowTitle();
+            isMonitoringChanging = true;
+        }
+
+        private bool? ConfirmDiscordChanging()
+        {
+            var result = MessageBox.Show(
+                $"{fileName ?? "untitled"} への変更内容を保存しますか?",
+                "YMF825 MIDI Driver",
+                MessageBoxButton.YesNoCancel, MessageBoxImage.None, MessageBoxResult.Yes);
+
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    return true;
+
+                case MessageBoxResult.No:
+                    return false;
+
+                default:
+                    return null;
+            }
+        }
+
+        private void NewCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (fileChanged)
+            {
+                var confirm = ConfirmDiscordChanging();
+
+                switch (confirm)
+                {
+                    case true:
+                        if (filePath == null)
+                        {
+                            if (ShowSaveDialog())
+                                SaveProject();
+                            else
+                                return;
+                        }
+                        else
+                            SaveProject();
+                        break;
+
+                    case null:
+                        return;
+                }
+            }
+
+            // reset tones
+            isMonitoringChanging = false;
+            toneItems.Clear();
+            filePath = null;
+            fileName = null;
+            fileChanged = false;
+            UpdateWindowTitle();
+            isMonitoringChanging = true;
+        }
+
+        private void CloseCommand_OnExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (fileChanged)
+            {
+                var confirm = ConfirmDiscordChanging();
+
+                switch (confirm)
+                {
+                    case true:
+                        if (filePath == null)
+                        {
+                            if (ShowSaveDialog())
+                                SaveProject();
+                            else
+                            {
+                                e.Cancel = true;
+                                return;
+                            }
+                        }
+                        else
+                            SaveProject();
+                        break;
+
+                    case null:
+                        e.Cancel = true;
+                        return;
+                }
+            }
+        }
+
+        #endregion
+
+
     }
 }
