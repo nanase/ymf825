91e9abfa71dee0f72b0e122798134e7e160d8a33
Tomona Nanase <nanase@users.noreply.github.com>
2017-10-25 23:40:47 +0900

    ディープコピーメソッド追加

---
+++ Ymf825MidiDriver/Extension.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Ymf825MidiDriver
+{
+    internal static class Extension
+    {
+        public static T DeepClone<T>(this T @object)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var bf = new BinaryFormatter();
+                bf.Serialize(ms, @object);
+                ms.Position = 0;
+                return (T)bf.Deserialize(ms);
+            }
+        }
+    }
+}
