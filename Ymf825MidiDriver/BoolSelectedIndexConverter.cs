e7675c34b01228f9d32eaac214bb72ef86260dbf
Tomona Nanase <nanase@users.noreply.github.com>
2017-10-25 23:43:11 +0900

    コンバータクラスを追加

---
+++ Ymf825MidiDriver/BoolSelectedIndexConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Ymf825MidiDriver
+{
+    [ValueConversion(typeof(object), typeof(bool))]
+    internal class BoolSelectedValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ Ymf825MidiDriver/ToneNumberToStringConveter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Ymf825MidiDriver
+{
+    [ValueConversion(typeof(object[]), typeof(string))]
+    internal class ToneNumberToStringConveter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 4)
+                return Binding.DoNothing;
+
+            if (!(values[0] is bool programNumberAssigned) || !(values[2] is bool percussionNumberAssigned) ||
+                !(values[1] is int programNumber) || !(values[3] is int percussionNumber))
+                return Binding.DoNothing;
+            
+            return programNumberAssigned ?
+                $"prog: #{programNumber}" + (percussionNumberAssigned ? $", perc: #{percussionNumber}" : "") :
+                "prog: #--";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return new[] { Binding.DoNothing, Binding.DoNothing };
+        }
+    }
+}
