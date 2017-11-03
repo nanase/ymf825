ea90e2b832391ce99cfcac9eeb5d15c906214795
Tomona Nanase <nanase@users.noreply.github.com>
2017-11-03 13:31:00 +0900

    XMLドキュメント追加

--- Ymf825/DataTransferedEventArgs.cs
+++ Ymf825/DataTransferedEventArgs.cs
@@ -2,20 +2,40 @@
 
 namespace Ymf825
 {
+    /// <inheritdoc />
+    /// <summary>
+    /// デバイスにデータが送信または受信されたときに発生するイベントの引数を格納します。
+    /// </summary>
     public class DataTransferedEventArgs : EventArgs
     {
         #region -- Public Properties --
 
+        /// <summary>
+        /// 送信先または受信元のデバイスを表す <see cref="Ymf825.TargetDevice"/> 列挙体から構成されるフラグ値を取得します。
+        /// </summary>
         public TargetDevice TargetDevice { get; }
 
+        /// <summary>
+        /// データアドレスを取得します。
+        /// </summary>
         public byte Address { get; }
 
+        /// <summary>
+        /// 送信または受信されたデータを取得します。
+        /// </summary>
         public byte Data { get; }
 
         #endregion
 
         #region -- Constructors --
 
+        /// <inheritdoc />
+        /// <summary>
+        /// 引数を指定して新しい <see cref="T:Ymf825.DataTransferedEventArgs" /> クラスのインスタンスを初期化します。
+        /// </summary>
+        /// <param name="targetDevice">送信先または受信元のデバイスを表す <see cref="Ymf825.TargetDevice"/> 列挙体から構成されるフラグ値。</param>
+        /// <param name="address">データアドレス。</param>
+        /// <param name="data">送信または受信されたデータ。</param>
         public DataTransferedEventArgs(TargetDevice targetDevice, byte address, byte data)
         {
             TargetDevice = targetDevice;
--- Ymf825/TargetDevice.cs
+++ Ymf825/TargetDevice.cs
@@ -2,12 +2,26 @@
 
 namespace Ymf825
 {
+    /// <summary>
+    /// データを送信または受信するデバイスを指定するための列挙体です。
+    /// 送信時は複数のデバイスを指定し、受信時は 1 つのデバイスのみ指定できます。
+    /// </summary>
     [Flags]
     public enum TargetDevice : byte
     {
+        /// <summary>
+        /// デバイスなし。
+        /// </summary>
         None = 0x00,
 
+        /// <summary>
+        /// 1つ目のデバイス。
+        /// </summary>
         Ymf825Board0 = 0x01,
+
+        /// <summary>
+        /// 2つ目のデバイス。
+        /// </summary>
         Ymf825Board1 = 0x02
     }
 }
