724d2d3b794477b52390fd586336c12665a8ad86
Tomona Nanase <nanase@users.noreply.github.com>
2017-12-10 17:49:28 +0900

    サンプルプログラムを更新
    セクションモード対応、音域の拡張

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -17,41 +17,14 @@ namespace TestConsoleApp
             using (var ymf825 = new CbwYmf825Bb(0))
             {
                 var driver = new Ymf825Driver(ymf825);
+                driver.EnableSectionMode();
+
                 Console.WriteLine("Software Reset");
                 driver.ResetSoftware();
-
-                for (var i = 0; i < 256; i++)
+                
                 {
-                    ymf825.ChangeTargetDevice(TargetDevice.Board0 | TargetDevice.Board1);
-                    driver.SetSoftwareTestCommunication((byte)i);
-                    var k0 = driver.GetSoftwareTestCommunication(TargetDevice.Board0);
-                    var k1 = driver.GetSoftwareTestCommunication(TargetDevice.Board1);
-
-                    if (i != k0 || i != k1)
-                        throw new Exception();
-                }
-
-                var r = new Random();
-                for (var i = 0; i < 256; i++)
-                {
-                    var q = r.Next(0, 256);
-                    ymf825.ChangeTargetDevice(TargetDevice.Board0 | TargetDevice.Board1);
-                    driver.SetSoftwareTestCommunication((byte)q);
-                    var k0 = driver.GetSoftwareTestCommunication(TargetDevice.Board0);
-                    var k1 = driver.GetSoftwareTestCommunication(TargetDevice.Board1);
-
-                    if (q != k0 || q != k1)
-                        throw new Exception();
-                }
-
-                ymf825.ChangeTargetDevice(TargetDevice.Board0 | TargetDevice.Board1);
-
-                {
-                    var toneBuffer = new byte[512];
-                    int toneBufferSize;
-                    
+                    var tones = new ToneParameterCollection();
                     {
-                        var tones = new ToneParameterCollection();
                         var tone = tones[0];
                         tone.BasicOctave = 0;
                         tone.LfoFrequency = 2;
@@ -96,44 +69,61 @@ namespace TestConsoleApp
                         tone.Operator4.EnableVibrato = true;
                         tone.Operator4.MagnificationOfFrequency = 2;
                         tone.Operator4.WaveShape = 0;
-
-                        toneBufferSize = tones.Export(toneBuffer, 0, 0);
                     }
 
                     Console.WriteLine("Tone Init");
-                    driver.WriteContentsData(toneBuffer, 0, toneBufferSize);
-                    driver.SetSequencerSetting(SequencerSetting.AllKeyOff | SequencerSetting.AllMute | SequencerSetting.AllEgReset |
-                                               SequencerSetting.R_FIFOR | SequencerSetting.R_SEQ | SequencerSetting.R_FIFO);
-                    driver.SleepAction(1);
-                    driver.SetSequencerSetting(SequencerSetting.Reset);
 
-                    driver.SetToneFlag(0, false, true, true);
-                    driver.SetChannelVolume(28, true);
-                    driver.SetVibratoModuration(0);
-                    driver.SetFrequencyMultiplier(1, 0);
+                    driver.Section(() =>
+                    {
+                        driver.WriteContentsData(tones, 0);
+                        driver.SetSequencerSetting(SequencerSetting.AllKeyOff | SequencerSetting.AllMute | SequencerSetting.AllEgReset |
+                                                   SequencerSetting.R_FIFOR | SequencerSetting.R_SEQ | SequencerSetting.R_FIFO);
+                    }, 1);
+
+                    driver.Section(() =>
+                    {
+                        driver.SetSequencerSetting(SequencerSetting.Reset);
+
+                        driver.SetToneFlag(0, false, true, true);
+                        driver.SetChannelVolume(28, true);
+                        driver.SetVibratoModuration(0);
+                        driver.SetFrequencyMultiplier(1, 0);
+                    });
                 }
 
                 var noteon = new Action<int>(key =>
                 {
                     Ymf825Driver.GetFnumAndBlock(key, out var fnum, out var block, out var correction);
                     Ymf825Driver.ConvertForFrequencyMultiplier(correction, out var integer, out var fraction);
+                    var freq = Ymf825Driver.CalcFrequency(fnum, block);
+                    Console.WriteLine("key: {0}, freq: {4:f1} Hz, fnum: {5:f0}, block: {6}, correction: {1:f3}, integer: {2}, fraction: {3}", key, correction, integer, fraction, freq, fnum, block);
 
-                    Console.WriteLine("key: {0}, correction: {1:f6}, integer: {2}, fraction: {3}", key, correction, integer, fraction);
-
-                    driver.SetVoiceNumber(0);
-                    driver.SetVoiceVolume(15);
-                    driver.SetFrequencyMultiplier(integer, fraction);
-                    driver.SetFnumAndBlock((int)Math.Round(fnum), block);
-                    driver.SetToneFlag(0, true, false, false);
+                    driver.Section(() =>
+                    {
+                        driver.SetVoiceNumber(0);
+                        driver.SetVoiceVolume(15);
+                        driver.SetFrequencyMultiplier(integer, fraction);
+                        driver.SetFnumAndBlock((int)Math.Round(fnum), block);
+                        driver.SetToneFlag(0, true, false, false);
+                    });
                 });
 
-                var noteoff = new Action(() => driver.SetToneFlag(0, false, false, false));
+                var noteoff = new Action(() =>
+                {
+                    driver.Section(() => driver.SetToneFlag(0, false, false, false));
+                });
 
                 var index = 0;
-                var score = new[] { 60, 62, 64, 65, 67, 69, 71, 72, 72, 71, 69, 67, 65, 64, 62, 60 };
+                var score = new[]
+                {
+                    60, 62, 64, 65, 67, 69, 71, 72,
+                    72, 74, 76, 77, 79, 81, 83, 84,
+                    84, 83, 81, 79, 77, 76, 74, 72,
+                    72, 71, 69, 67, 65, 64, 62, 60
+                };
                 while (true)
                 {
-                    const int noteOnTime = 500;
+                    const int noteOnTime = 250;
                     const int sleepTime = 0;
 
                     noteon(score[index]);
