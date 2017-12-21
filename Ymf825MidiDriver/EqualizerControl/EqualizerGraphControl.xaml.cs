98d7fb77f7c89f49865d408ebc3285e4b761892e
Tomona Nanase <nanase@users.noreply.github.com>
2017-12-21 18:17:40 +0900

    イコライザブロック図コントロールを追加

---
+++ Ymf825MidiDriver/EqualizerControl/EqualizerGraphControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ymf825MidiDriver.EqualizerControl
+{
+    /// <inheritdoc cref="UserControl" />
+    /// <summary>
+    /// EqualizerGraphControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class EqualizerGraphControl
+    {
+        #region -- Private Fields --
+
+        public static readonly DependencyProperty EqualizerItemProperty = DependencyProperty.Register(nameof(EqualizerItem), typeof(EqualizerItem), typeof(EqualizerGraphControl), new PropertyMetadata(default(EqualizerItem)));
+
+        #endregion
+
+        #region -- Public Properties --
+
+        public EqualizerItem EqualizerItem
+        {
+            // ReSharper disable once PossibleNullReferenceException
+            get => (EqualizerItem)GetValue(EqualizerItemProperty);
+            set => SetValue(EqualizerItemProperty, value);
+        }
+
+        #endregion
+
+        #region -- Constructors --
+
+        public EqualizerGraphControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
