10b27d19e289a51b4a43c417b149e1149d6e5b5c
Tomona Nanase <nanase@users.noreply.github.com>
2018-08-05 10:56:08 +0900

    未使用のusingを削除

--- Ymf825/Driver/Ymf825Driver.static.cs
+++ Ymf825/Driver/Ymf825Driver.static.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 
 namespace Ymf825.Driver
 {
--- Ymf825/IO/D2xxSpiPinConfig.cs
+++ Ymf825/IO/D2xxSpiPinConfig.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ymf825.IO
+﻿namespace Ymf825.IO
 {
     public class D2xxSpiPinConfig
     {
